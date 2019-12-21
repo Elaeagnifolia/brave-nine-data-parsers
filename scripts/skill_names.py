343de986250370bc764c0885bc1ffb553b85ed5e
Elaeagnifolia <j.wong205914@gmail.com>
2019-12-20 22:29:44 -0600

    Basic fixes to skill names script

--- scripts/skill_names.py
+++ scripts/skill_names.py
@@ -1,20 +1,19 @@
 import xml.etree.ElementTree as ET
 import os
 
-skills = ET.parse('SkillBasicListText_ENG.txt')
+def main():
+	skills = ET.parse('SkillBasicListText_ENG.txt')
 
-def findCode(code):
-        root = skills.getroot()
-        for child in root:
-                if child.find('_code').text == code:
-                        text = removeExtraText(child.find('_text').text)
-                        return text
+	file = open("skill-names.txt", "w", encoding="utf-8")
+	tooltip = ET.parse('SkillTooltipDataBasicList.txt')
+	root = skills.getroot()
+	for child in root:
+		# We only want to get the codes ending in "71" and with
+		# length greater than 5 since those are the skill names
+	    if child.find('_code').text[-2:] == "71" and len(child.find('_code').text) > 5:
+	        file.write(child.find('_text').text)
+	        file.write('\n')
+	file.close()
 
-file = open("skill-names.txt", "w", encoding="utf-8")
-tooltip = ET.parse('SkillTooltipDataBasicList.txt')
-root = skills.getroot()
-for child in root:
-    if child.find('_code').text[-2:] == "71" and len(child.find('_code').text) > 5:
-        file.write(child.find('_text').text)
-        file.write('\n')
-file.close()
+if __name__ == "__main__":
+	main()
